696849b4a70fa29b4b650c042d9a77e5ec589a60
Kunz Robert <robert_kunz@sluz.ch>
2024-06-08 13:33:00 +0200

    Add homepage UI

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonIcon,
+  IonItemDivider
+} from '@ionic/angular/standalone';
+import {addIcons} from "ionicons";
+import {cameraOutline, locationOutline} from "ionicons/icons";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonItemDivider],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons({ cameraOutline, locationOutline })
+  }
 }
