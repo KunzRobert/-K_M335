110ce06360042460d9e90d3ab7d3bc6d01a9ae49
Christian Hofmann <c.hofmann@live.de>
2024-06-08 15:01:27 +0200

    Implement request access to camera

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,25 +1,42 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {
-  IonHeader,
-  IonToolbar,
-  IonTitle,
-  IonContent,
   IonButton,
+  IonContent,
+  IonHeader,
   IonIcon,
-  IonItemDivider
+  IonItemDivider,
+  IonTitle,
+  IonToolbar
 } from '@ionic/angular/standalone';
 import {addIcons} from "ionicons";
 import {cameraOutline, locationOutline} from "ionicons/icons";
+import {Camera} from '@capacitor/camera';
+import {AsyncPipe, NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonItemDivider],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonItemDivider, AsyncPipe, NgClass],
 })
 export class HomePage {
+
+  hasCameraPermissions = false;
+
   constructor() {
-    addIcons({ cameraOutline, locationOutline })
+    addIcons({cameraOutline, locationOutline})
+
+    Camera.checkPermissions()
+      .then(c => this.hasCameraPermissions = c.camera === 'granted');
+  }
+
+  async requestPermissions(): Promise<void> {
+    Camera.requestPermissions({permissions: ['camera']})
+      .then(res => this.hasCameraPermissions = res.camera === 'granted')
+  }
+
+  hasCameraAccess(): boolean {
+    return this.hasCameraPermissions;
   }
 }
